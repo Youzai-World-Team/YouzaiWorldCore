134e76aa28b7474761113586f71867936fcd724b
JessDaodao <694428809@qq.com>
2026-05-01 19:45:25 +0800

    init

---
+++ src/client/java/top/csituka/youzaiworldcore/client/YouzaiworldCoreClient.java
@@ -0,0 +1,10 @@
+package top.csituka.youzaiworldcore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YouzaiworldCoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/top/csituka/youzaiworldcore/YouzaiworldCore.java
@@ -0,0 +1,10 @@
+package top.csituka.youzaiworldcore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class YouzaiworldCore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
