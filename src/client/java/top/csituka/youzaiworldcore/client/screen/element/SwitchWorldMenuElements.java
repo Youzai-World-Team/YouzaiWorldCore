b75118110c4926d3c5ad06fb5fd748db4ab6efaa
JessDaodao <694428809@qq.com>
2026-05-02 12:26:26 +0800

    完善

--- src/client/java/top/csituka/youzaiworldcore/client/screen/element/SwitchWorldMenuElements.java
+++ src/client/java/top/csituka/youzaiworldcore/client/screen/element/SwitchWorldMenuElements.java
@@ -31,65 +31,47 @@ public class SwitchWorldMenuElements implements MenuElementGroup {
         int centerX = screenWidth / 2;
         int centerY = screenHeight / 2;
 
-        float scaledLargeW = LARGE_BUTTON_WIDTH * scale;
-        float scaledLargeH = LARGE_BUTTON_HEIGHT * scale;
-        float scaledSmallW = SMALL_BUTTON_WIDTH * scale;
-        float scaledSmallH = SMALL_BUTTON_HEIGHT * scale;
-        float scaledNarrowW = NARROW_BUTTON_WIDTH * scale;
-        float scaledWideW = WIDE_BUTTON_WIDTH * scale;
+        float scaledLeftW = LARGE_BUTTON_WIDTH * scale;
+        float scaledRightW = SMALL_BUTTON_WIDTH * scale;
+        float scaledTopH = LARGE_BUTTON_HEIGHT * scale;
         float scaledBottomH = BOTTOM_BUTTON_HEIGHT * scale;
         float scaledSpacing = BUTTON_SPACING * scale;
         float scaledRowSpacing = ROW_SPACING * scale;
 
-        float topRowWidth = scaledLargeW + scaledSpacing + scaledSmallW;
-        float startX = centerX - topRowWidth / 2;
-
-        float largeButtonY = centerY - scaledLargeH / 2 - scaledRowSpacing;
+        float totalWidth = scaledLeftW + scaledSpacing + scaledRightW;
+        float startX = centerX - totalWidth / 2;
+        float topY = centerY - scaledTopH / 2 - scaledRowSpacing;
 
         TransparentButton topLeft = new TransparentButton(
-                (int) startX, (int) largeButtonY, (int) scaledLargeW, (int) scaledLargeH,
+                (int) startX, (int) topY, (int) scaledLeftW, (int) scaledTopH,
                 Component.translatable("screen.youzaiworldcore.switch_world.world1"),
                 () -> {}
         );
         topLeft.setExternalAlpha(alpha);
         buttons.add(topLeft);
 
-        float rightButtonsX = startX + scaledLargeW + scaledSpacing;
-        float totalSmallHeight = scaledSmallH * 2 + scaledSpacing;
-        float smallButtonsStartY = centerY - totalSmallHeight / 2 - scaledRowSpacing;
-
-        TransparentButton topRightUpper = new TransparentButton(
-                (int) rightButtonsX, (int) smallButtonsStartY, (int) scaledSmallW, (int) scaledSmallH,
+        TransparentButton topRight = new TransparentButton(
+                (int) (startX + scaledLeftW + scaledSpacing), (int) topY, (int) scaledRightW, (int) scaledTopH,
                 Component.translatable("screen.youzaiworldcore.switch_world.world2"),
                 () -> {}
         );
-        topRightUpper.setExternalAlpha(alpha);
-        buttons.add(topRightUpper);
+        topRight.setExternalAlpha(alpha);
+        buttons.add(topRight);
 
-        TransparentButton topRightLower = new TransparentButton(
-                (int) rightButtonsX, (int) (smallButtonsStartY + scaledSmallH + scaledSpacing), (int) scaledSmallW, (int) scaledSmallH,
-                Component.translatable("screen.youzaiworldcore.switch_world.world3"),
-                () -> {}
-        );
-        topRightLower.setExternalAlpha(alpha);
-        buttons.add(topRightLower);
-
-        float bottomRowY = largeButtonY + scaledLargeH + scaledRowSpacing;
-        float bottomRowWidth = scaledNarrowW + scaledSpacing + scaledWideW;
-        float bottomStartX = centerX - bottomRowWidth / 2;
+        float bottomY = topY + scaledTopH + scaledRowSpacing;
 
         TransparentButton bottomLeft = new TransparentButton(
-                (int) bottomStartX, (int) bottomRowY, (int) scaledNarrowW, (int) scaledBottomH,
-                Component.translatable("screen.youzaiworldcore.switch_world.world4"),
+                (int) startX, (int) bottomY, (int) scaledLeftW, (int) scaledBottomH,
+                Component.translatable("screen.youzaiworldcore.switch_world.world3"),
                 () -> {}
         );
         bottomLeft.setExternalAlpha(alpha);
         buttons.add(bottomLeft);
 
         TransparentButton bottomRight = new TransparentButton(
-                (int) (bottomStartX + scaledNarrowW + scaledSpacing), (int) bottomRowY, (int) scaledWideW, (int) scaledBottomH,
-                Component.translatable("screen.youzaiworldcore.switch_world.back"),
-                () -> screen.goBack()
+                (int) (startX + scaledLeftW + scaledSpacing), (int) bottomY, (int) scaledRightW, (int) scaledBottomH,
+                Component.translatable("screen.youzaiworldcore.switch_world.world4"),
+                () -> {}
         );
         bottomRight.setExternalAlpha(alpha);
         buttons.add(bottomRight);
